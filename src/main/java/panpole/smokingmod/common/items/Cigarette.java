f4d38b85d048f8ff2e076b44e92c2e4ed1418d53
PanPole <77209709+PanPolePL@users.noreply.github.com>
2023-02-16 17:42:56 +0100

    uploaded

---
+++ src/main/java/panpole/smokingmod/common/items/Cigarette.java
@@ -0,0 +1,29 @@
+package panpole.smokingmod.common.items;
+
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+public class Cigarette extends Item
+{
+    public Cigarette(Properties properties)
+    {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand)
+    {
+        player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 2));
+        for(int i=0; i<5; i++)
+        {
+            world.addAlwaysVisibleParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, player.getX(), player.getY()+1.5, player.getZ(), 0, 0, 0);
+        }
+        return super.use(world, player, hand);
+    }
+}
